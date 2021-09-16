d93e212f3425211f4c505f9faffb38feadd2dc0c
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-15 21:38:08 -0300

    Criada a classe Posição

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Tabuleiro;
 
 namespace Xadrez_Console
 {
@@ -10,6 +7,12 @@ namespace Xadrez_Console
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+            Console.ReadLine();
         }
     }
 }
