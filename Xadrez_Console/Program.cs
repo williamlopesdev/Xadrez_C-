66fffc98cb911976826a46d54f420be6dc614d7a
FUNCIONAL\william.malaquias <william.malaquias@funcionalcorp.com.br>
2021-09-19 09:01:44 -0300

    primeiros testes de movimentação

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -11,8 +11,22 @@ namespace Xadrez_Console
             try
             {
                 PartidaDeXadrez partida = new PartidaDeXadrez();
-                
-                Tela.imprimirTabuleiro(partida.tab);
+
+                while (!partida.terminada) {
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.ExecutaMovimento(origem, destino);
+                }
+
             }
             catch (TabuleiroException e)
             {
